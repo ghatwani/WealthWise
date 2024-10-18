7f991653efb15bd4018cde74d776498136e9c57a
Divya Ghatwani <210303125093@paruluniversity.ac.in>
2024-10-18 18:41:55 +0530

    datavase connected

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
+import { connectDB } from "./utils/feature";
 const app = express()
 const port = 3000
 require('dotenv').config()
 const mongo_uri=process.env.MONGO_URI
+connectDB(mongo_uri)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
