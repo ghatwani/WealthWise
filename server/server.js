d078f9a093e65fb273efef93716b7ba1dbb058f7
Divya Ghatwani <210303125093@paruluniversity.ac.in>
2024-10-18 18:06:38 +0530

    made changes

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const port = 3000
 
 const mongo_uri='mongodb+srv://dghatwani890:Bqo6ramqXH9T3hDp@cluster0.qz7j6.mongodb.net/'
 
-app.get('/', (req, res) => {
+app.get('/',(req, res) => {
   res.send('Hello World!')
 })
 
