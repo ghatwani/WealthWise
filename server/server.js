770d0bb8b479265276382c34fc51cf6659f8667a
Priyanshu <priyanshu saxena@LAPTOP-BC9BUENT>
2024-10-18 18:45:21 +0530

    done

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
-require('dotenv').config()
-const mongo_uri=process.env.MONGO_URI
+import express from "express";
+import dotenv from "dotenv";
+const app = express();
+const port = 3000;
+dotenv.config();
+const mongo_uri = process.env.MONGO_URI;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
