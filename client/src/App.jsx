4a0cccbede4a50d9b6a47415afb6fd23b571c998
Priyanshu <priyanshu saxena@LAPTOP-BC9BUENT>
2024-11-08 19:05:39 +0530

    Modified recommendation system

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import LandingPage from './pages/LandingPage';
-import SignInPage from './pages/SignInPage';
-import SignUpPage from './pages/SignUpPage';
-import HomePage from './pages/HomePage';
-import AddTransaction from './pages/AddTransaction';
-import {Toaster} from 'react-hot-toast'
-import SmartDealPage from './pages/SmartDealPage';
-=======
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AOS from "aos";
@@ -22,7 +9,7 @@ import HomePage from "./pages/HomePage";
 import AddTransaction from "./pages/AddTransaction";
 import { Toaster } from "react-hot-toast";
 import ChatBot from "./pages/Chatbot.jsx";
->>>>>>> eb127ec89f698547320c8a78e046cf3245b7837f
+import SmartDealPage from "./pages/SmartDealPage.jsx";
 
 function App() {
   useEffect(() => {
@@ -40,11 +27,8 @@ function App() {
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/add-transaction" element={<AddTransaction />} />
-<<<<<<< HEAD
         <Route path="/smart-deal" element={<SmartDealPage />} />
-=======
         <Route path="/chat" element={<ChatBot />} />
->>>>>>> eb127ec89f698547320c8a78e046cf3245b7837f
       </Routes>
       <Toaster />
     </Router>
