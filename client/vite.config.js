9079d2265581a3ed43c3a9d1bd4c8a56c1013d31
Divya Ghatwani <210303125093@paruluniversity.ac.in>
2024-11-10 23:15:30 +0530

    abc

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://65.20.75.57:5000",
       },
     },
   },
